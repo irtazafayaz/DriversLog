593c45093aa31c1592456daa54571f3df27af5d9
Irtaza Fiaz <irtazab135@gmail.com>
2024-03-11 13:22:19 +0500

    Project Setup

---
+++ Drivers Log/Drivers_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drivers_LogApp.swift
+//  Drivers Log
+//
+//  Created by Irtaza Fiaz on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Drivers_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
