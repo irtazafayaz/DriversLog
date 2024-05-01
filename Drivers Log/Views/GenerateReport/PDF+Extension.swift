7e6929ae0cd0f6043b129774356826952ba0132a
irtazafayaz <irtazab135@gmail.com>
2024-05-01 20:56:37 +0500

    --pdf formatting

---
+++ Drivers Log/Views/GenerateReport/PDF+Extension.swift
@@ -0,0 +1,102 @@
+//
+//  PDF+Extension.swift
+//  Drivers Log
+//
+//  Created by Irtaza Fiaz on 01/05/2024.
+//
+
+import SwiftUI
+import UIKit
+
+extension PDFFormattedView {
+    
+    func drawDeclaration(ctx: UIGraphicsPDFRendererContext, startX: CGFloat, startY: CGFloat, pageRect: CGRect, title: String, details: String) {
+        let margin: CGFloat = 20
+        let titleFrame = CGRect(x: startX, y: startY, width: pageRect.width - 2 * margin, height: 20)
+        ctx.cgContext.setFillColor(UIColor.systemGreen.cgColor)
+        ctx.cgContext.fill(titleFrame)
+        drawText(title, in: titleFrame, withAlignment: .center, fontSize: 10, isBold: true, backgroundColor: .clear, textColor: .white)
+        
+        let detailsFrame = CGRect(x: startX, y: startY + 20, width: pageRect.width - 2 * margin, height: 40)
+        drawText(details, in: detailsFrame, withAlignment: .left, fontSize: 10)
+    }
+    
+    func drawSignature(ctx: UIGraphicsPDFRendererContext, startX: CGFloat, startY: CGFloat, pageRect: CGRect) {
+        let margin: CGFloat = 20
+        let signatureText = "Signature: ________________    Date: ________________"
+        let signatureFrame = CGRect(x: startX, y: startY, width: pageRect.width - 2 * margin, height: 20)
+        drawText(signatureText, in: signatureFrame, withAlignment: .left, fontSize: 10)
+    }
+    
+    func drawText(_ text: String, in rect: CGRect, withAlignment alignment: NSTextAlignment, fontSize: CGFloat, isBold: Bool = false, backgroundColor: UIColor = .white, textColor: UIColor = .black) {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = alignment
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: isBold ? UIFont.boldSystemFont(ofSize: fontSize) : UIFont.systemFont(ofSize: fontSize),
+            .paragraphStyle: paragraphStyle,
+            .foregroundColor: textColor,
+            .backgroundColor: backgroundColor
+        ]
+        text.draw(in: rect, withAttributes: attributes)
+    }
+    
+
+    
+    
+    func savePDF(data: Data) {
+        if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let pdfPath = documentDirectory.appendingPathComponent("DayTimeLog2page.pdf")
+            do {
+                try data.write(to: pdfPath)
+                print("PDF created at: \(pdfPath)")
+                sharePDF(pdfPath: pdfPath)
+            } catch {
+                print("Could not save PDF: \(error)")
+            }
+        }
+    }
+    
+    func sharePDF(pdfPath: URL) {
+        let activityViewController = UIActivityViewController(activityItems: [pdfPath], applicationActivities: nil)
+        
+        // Presenting the share sheet
+        if let topController = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.rootViewController {
+            // Ensures that iPad doesn't crash on attempting to present a full screen VC in a popover
+            activityViewController.popoverPresentationController?.sourceView = topController.view
+            
+            topController.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func totalHoursAndDistance(trips: [TripItem], filter: (TripItem) -> Bool) -> (hours: Double, distance: Double) {
+        var totalHours = 0.0000
+        var totalDistance = 0.0000
+
+        for trip in trips.filter(filter) {
+            // Parse duration string "HH:mm:ss" to calculate total hours
+            let durationComponents = trip.duration.split(separator: ":").map { Double($0) ?? 0.0 }
+            if durationComponents.count == 3 {
+                let hours = durationComponents[0]
+                let minutes = durationComponents[1] / 60
+                let seconds = durationComponents[2] / 3600
+                totalHours += hours + minutes + seconds
+            }
+
+            // Use the distance directly as it's assumed to be in kilometers
+            if let distance = Double(trip.tripTotalDistance) {
+                totalDistance += distance
+            }
+        }
+
+        return (totalHours, totalDistance)
+    }
+
+    func formatHours(_ hours: Double) -> String {
+        return String(format: "%.2fh", hours)
+    }
+
+    func formatDistance(_ distance: Double) -> String {
+        return String(format: "%.2fkm", distance)
+    }
+    
+}
--- Drivers Log/Views/GenerateReport/PDFFormattedView.swift
+++ Drivers Log/Views/GenerateReport/PDFFormattedView.swift
@@ -77,36 +77,114 @@ struct PDFFormattedView: View {
         savePDF(data: data)
     }
     
-    func totalHoursAndDistance(trips: [TripItem], filter: (TripItem) -> Bool) -> (hours: Double, distance: Double) {
-        var totalHours = 0.0000
-        var totalDistance = 0.0000
-
-        for trip in trips.filter(filter) {
-            // Parse duration string "HH:mm:ss" to calculate total hours
-            let durationComponents = trip.duration.split(separator: ":").map { Double($0) ?? 0.0 }
-            if durationComponents.count == 3 {
-                let hours = durationComponents[0]
-                let minutes = durationComponents[1] / 60
-                let seconds = durationComponents[2] / 3600
-                totalHours += hours + minutes + seconds
-            }
-
-            // Use the distance directly as it's assumed to be in kilometers
-            if let distance = Double(trip.tripTotalDistance) {
-                totalDistance += distance
+    func drawTable(at point: CGPoint, in context: CGContext, contentRect: CGRect) {
+        let headers = ["Date", "Start", "Finish", "Duration", "Distance", "From", "To", "Road", "Weather", "Traffic"]
+        let rowHeight: CGFloat = 20.0
+        let columnWidth = (contentRect.width) / CGFloat(headers.count)
+        
+        context.saveGState()
+        context.translateBy(x: point.x, y: point.y)
+        
+        // Draw header
+        for (index, header) in headers.enumerated() {
+            let headerAttributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.boldSystemFont(ofSize: 12),
+                .foregroundColor: UIColor.white
+            ]
+            let frame = CGRect(x: CGFloat(index) * columnWidth, y: 0, width: columnWidth, height: rowHeight)
+            context.setFillColor(UIColor.systemGreen.cgColor)
+            context.fill(frame)
+            let textRect = frame.insetBy(dx: 2, dy: 2)
+            header.draw(in: textRect, withAttributes: headerAttributes)
+        }
+        
+        // Draw rows dynamically based on trips data
+        for (rowIndex, trip) in trips.enumerated() {
+            let rowItems = [
+                trip.tripDate,
+                trip.startTime,
+                trip.endTime,
+                trip.duration,
+                trip.tripTotalDistance,
+                trip.startLocation,
+                trip.endLocation,
+                "Paved",
+                "Sunny",
+                "Light"
+            ]
+            
+            for (columnIndex, item) in rowItems.enumerated() {
+                let itemAttributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.systemFont(ofSize: 12),
+                    .foregroundColor: UIColor.black
+                ]
+                let frame = CGRect(x: CGFloat(columnIndex) * columnWidth, y: CGFloat(rowIndex + 1) * rowHeight, width: columnWidth, height: rowHeight)
+                context.setFillColor(UIColor.white.cgColor)
+                context.fill(frame)
+                let textRect = frame.insetBy(dx: 2, dy: 2)
+                item.draw(in: textRect, withAttributes: itemAttributes)
             }
         }
+        
+        context.restoreGState()
+    }
+    
 
-        return (totalHours, totalDistance)
+    func drawAbbreviations(ctx: UIGraphicsPDFRendererContext, pageRect: CGRect, margin: CGFloat) {
+        let startY = margin
+        let columnWidths = [100.0, 50.0, 100.0, 50.0, 100.0, 50.0].map { CGFloat($0) }
+        let rowHeight: CGFloat = 20.0
+        let startX = (pageRect.width - columnWidths.reduce(0, +)) / 2
+        
+        let headers = ["Road Type", "Abb.", "Weather", "Abb.", "Traffic Density", "Abb."]
+        let data = [
+            ("Sealed", "S"), ("Wet", "W"), ("Light", "L"),
+            ("Unsealed", "U"), ("Dry", "D"), ("Medium", "M"),
+            ("Quiet Street", "Q"), ("", ""), ("Heavy", "H")
+        ]
+        
+        ctx.cgContext.saveGState()
+        ctx.cgContext.translateBy(x: startX, y: startY)
+        
+        // Draw headers with background
+        for (index, header) in headers.enumerated() {
+            let headerFrame = CGRect(x: CGFloat(columnWidths[0..<index].reduce(0, +)), y: 0, width: columnWidths[index], height: rowHeight)
+            ctx.cgContext.setFillColor(UIColor.systemGreen.cgColor)
+            ctx.cgContext.fill(headerFrame)
+            drawText(header, in: headerFrame, withAlignment: .center, fontSize: 10, isBold: true, backgroundColor: .clear, textColor: .white)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.stroke(headerFrame)
+        }
+        
+        // Draw data rows
+        var currentY = rowHeight
+        for (index, (text, abbreviation)) in data.enumerated() {
+            let colIndex = index % 3 * 2
+            let textFrame = CGRect(x: CGFloat(columnWidths[0..<colIndex].reduce(0, +)), y: currentY, width: columnWidths[colIndex], height: rowHeight)
+            let abbrFrame = CGRect(x: CGFloat(columnWidths[0..<(colIndex + 1)].reduce(0, +)), y: currentY, width: columnWidths[colIndex + 1], height: rowHeight)
+            
+            // Set background color for rows
+            ctx.cgContext.setFillColor(UIColor.lightGray.cgColor)
+            ctx.cgContext.fill(textFrame)
+            ctx.cgContext.fill(abbrFrame)
+            
+            // Draw text in the respective cells
+            drawText(text, in: textFrame, withAlignment: .left, fontSize: 10, backgroundColor: .clear)
+            drawText(abbreviation, in: abbrFrame, withAlignment: .center, fontSize: 10, backgroundColor: .clear)
+            
+            // Draw borders
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.stroke(textFrame)
+            ctx.cgContext.stroke(abbrFrame)
+            
+            if (index + 1) % 3 == 0 {
+                currentY += rowHeight
+            }
+        }
+        
+        ctx.cgContext.restoreGState()
     }
 
-    func formatHours(_ hours: Double) -> String {
-        return String(format: "%.2fh", hours)
-    }
-
-    func formatDistance(_ distance: Double) -> String {
-        return String(format: "%.2fkm", distance)
-    }
     
     func drawSummaryAndDeclarations(ctx: UIGraphicsPDFRendererContext, pageRect: CGRect, margin: CGFloat) {
         
@@ -177,163 +255,4 @@ struct PDFFormattedView: View {
     }
     
     
-    
-    
-    func drawDeclaration(ctx: UIGraphicsPDFRendererContext, startX: CGFloat, startY: CGFloat, pageRect: CGRect, title: String, details: String) {
-        let margin: CGFloat = 20
-        let titleFrame = CGRect(x: startX, y: startY, width: pageRect.width - 2 * margin, height: 20)
-        ctx.cgContext.setFillColor(UIColor.systemGreen.cgColor)
-        ctx.cgContext.fill(titleFrame)
-        drawText(title, in: titleFrame, withAlignment: .center, fontSize: 10, isBold: true, backgroundColor: .clear, textColor: .white)
-        
-        let detailsFrame = CGRect(x: startX, y: startY + 20, width: pageRect.width - 2 * margin, height: 40)
-        drawText(details, in: detailsFrame, withAlignment: .left, fontSize: 10)
-    }
-    
-    func drawSignature(ctx: UIGraphicsPDFRendererContext, startX: CGFloat, startY: CGFloat, pageRect: CGRect) {
-        let margin: CGFloat = 20
-        let signatureText = "Signature: ________________    Date: ________________"
-        let signatureFrame = CGRect(x: startX, y: startY, width: pageRect.width - 2 * margin, height: 20)
-        drawText(signatureText, in: signatureFrame, withAlignment: .left, fontSize: 10)
-    }
-    
-    func drawAbbreviations(ctx: UIGraphicsPDFRendererContext, pageRect: CGRect, margin: CGFloat) {
-        let startY = margin
-        let columnWidths = [100.0, 50.0, 100.0, 50.0, 100.0, 50.0].map { CGFloat($0) }
-        let rowHeight: CGFloat = 20.0
-        let startX = (pageRect.width - columnWidths.reduce(0, +)) / 2
-        
-        let headers = ["Road Type", "Abb.", "Weather", "Abb.", "Traffic Density", "Abb."]
-        let data = [
-            ("Sealed", "S"), ("Wet", "W"), ("Light", "L"),
-            ("Unsealed", "U"), ("Dry", "D"), ("Medium", "M"),
-            ("Quiet Street", "Q"), ("", ""), ("Heavy", "H")
-        ]
-        
-        ctx.cgContext.saveGState()
-        ctx.cgContext.translateBy(x: startX, y: startY)
-        
-        // Draw headers with background
-        for (index, header) in headers.enumerated() {
-            let headerFrame = CGRect(x: CGFloat(columnWidths[0..<index].reduce(0, +)), y: 0, width: columnWidths[index], height: rowHeight)
-            ctx.cgContext.setFillColor(UIColor.systemGreen.cgColor)
-            ctx.cgContext.fill(headerFrame)
-            drawText(header, in: headerFrame, withAlignment: .center, fontSize: 10, isBold: true, backgroundColor: .clear, textColor: .white)
-        }
-        
-        // Draw data rows
-        var currentY = rowHeight
-        for (index, (text, abbreviation)) in data.enumerated() {
-            let colIndex = index % 3 * 2
-            let textFrame = CGRect(x: CGFloat(columnWidths[0..<colIndex].reduce(0, +)), y: currentY, width: columnWidths[colIndex], height: rowHeight)
-            let abbrFrame = CGRect(x: CGFloat(columnWidths[0..<(colIndex + 1)].reduce(0, +)), y: currentY, width: columnWidths[colIndex + 1], height: rowHeight)
-            
-            // Set background color for rows
-            ctx.cgContext.setFillColor(UIColor.lightGray.cgColor)
-            ctx.cgContext.fill(textFrame)
-            ctx.cgContext.fill(abbrFrame)
-            
-            // Draw text in the respective cells
-            drawText(text, in: textFrame, withAlignment: .left, fontSize: 10, backgroundColor: .clear)
-            drawText(abbreviation, in: abbrFrame, withAlignment: .center, fontSize: 10, backgroundColor: .clear)
-            
-            if (index + 1) % 3 == 0 {
-                currentY += rowHeight
-            }
-        }
-        
-        ctx.cgContext.restoreGState()
-    }
-    
-    
-    
-    func drawText(_ text: String, in rect: CGRect, withAlignment alignment: NSTextAlignment, fontSize: CGFloat, isBold: Bool = false, backgroundColor: UIColor = .white, textColor: UIColor = .black) {
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = alignment
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: isBold ? UIFont.boldSystemFont(ofSize: fontSize) : UIFont.systemFont(ofSize: fontSize),
-            .paragraphStyle: paragraphStyle,
-            .foregroundColor: textColor,
-            .backgroundColor: backgroundColor
-        ]
-        text.draw(in: rect, withAttributes: attributes)
-    }
-    
-    func drawTable(at point: CGPoint, in context: CGContext, contentRect: CGRect) {
-        let headers = ["Date", "Start", "Finish", "Duration", "Distance", "From", "To", "Road", "Weather", "Traffic"]
-        let rowHeight: CGFloat = 20.0
-        let columnWidth = (contentRect.width) / CGFloat(headers.count)
-        
-        context.saveGState()
-        context.translateBy(x: point.x, y: point.y)
-        
-        // Draw header
-        for (index, header) in headers.enumerated() {
-            let headerAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont.boldSystemFont(ofSize: 12),
-                .foregroundColor: UIColor.white
-            ]
-            let frame = CGRect(x: CGFloat(index) * columnWidth, y: 0, width: columnWidth, height: rowHeight)
-            context.setFillColor(UIColor.systemGreen.cgColor)
-            context.fill(frame)
-            let textRect = frame.insetBy(dx: 2, dy: 2)
-            header.draw(in: textRect, withAttributes: headerAttributes)
-        }
-        
-        // Draw rows dynamically based on trips data
-        for (rowIndex, trip) in trips.enumerated() {
-            let rowItems = [
-                trip.tripDate,
-                trip.startTime,
-                trip.endTime,
-                trip.duration,
-                trip.tripTotalDistance,
-                trip.startLocation,
-                trip.endLocation,
-                "Paved",
-                "Sunny",
-                "Light"
-            ]
-            
-            for (columnIndex, item) in rowItems.enumerated() {
-                let itemAttributes: [NSAttributedString.Key: Any] = [
-                    .font: UIFont.systemFont(ofSize: 12),
-                    .foregroundColor: UIColor.black
-                ]
-                let frame = CGRect(x: CGFloat(columnIndex) * columnWidth, y: CGFloat(rowIndex + 1) * rowHeight, width: columnWidth, height: rowHeight)
-                context.setFillColor(UIColor.white.cgColor)
-                context.fill(frame)
-                let textRect = frame.insetBy(dx: 2, dy: 2)
-                item.draw(in: textRect, withAttributes: itemAttributes)
-            }
-        }
-        
-        context.restoreGState()
-    }
-    
-    
-    func savePDF(data: Data) {
-        if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let pdfPath = documentDirectory.appendingPathComponent("DayTimeLog2page.pdf")
-            do {
-                try data.write(to: pdfPath)
-                print("PDF created at: \(pdfPath)")
-                sharePDF(pdfPath: pdfPath)
-            } catch {
-                print("Could not save PDF: \(error)")
-            }
-        }
-    }
-    
-    func sharePDF(pdfPath: URL) {
-        let activityViewController = UIActivityViewController(activityItems: [pdfPath], applicationActivities: nil)
-        
-        // Presenting the share sheet
-        if let topController = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.rootViewController {
-            // Ensures that iPad doesn't crash on attempting to present a full screen VC in a popover
-            activityViewController.popoverPresentationController?.sourceView = topController.view
-            
-            topController.present(activityViewController, animated: true, completion: nil)
-        }
-    }
 }
